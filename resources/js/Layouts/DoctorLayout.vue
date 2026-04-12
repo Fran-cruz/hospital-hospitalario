dab5c1ad594d42f1673174b9537fd876e6a421c5
Fran-cruz <fraj_cruzf38@unicah.edu>
2026-04-11 18:47:03 -0600

    Agregar funcionalidad de cancelar y reprogramar citas para el administrador

--- resources/js/Layouts/DoctorLayout.vue
+++ resources/js/Layouts/DoctorLayout.vue
@@ -22,7 +22,7 @@ const isActive = (routeName) => route().current(routeName)
             permanent
             :width="240"
             :rail-width="60"
-            color="blue-darken-4" <!-- Cambio de color aquí -->
+            color="blue-darken-4"
         >
             <!-- Logo -->
             <v-list nav>
